2548c83398a3b15c69bfc219f48fd2d7232bf156
Michael Roker <mroker@gmail.com>
2016-05-30 16:41:18 -0400

    Added more exercise files

---
+++ three.rb
@@ -0,0 +1,12 @@
+movies = {:"star wars" => 1975,
+          :"LOTR" => 2004,
+	  :equilibrium => 2013,
+	  :gestures => 2001,
+	  :noman => 1981}
+
+
+puts movies[:"star wars"]
+puts movies[:"LOTR"]
+puts movies[:equilibrium]
+puts movies[:gestures]
+puts movies[:noman]
